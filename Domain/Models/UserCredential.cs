65a1e7dc9749208504c0493eab7b4e8c0204e1b5
Ulug'bek <1.ulugbekoqmonov@gmail.com>
2023-06-13 20:43:38 +0500

    Serilog has been corrected

--- Domain/Models/UserCredential.cs
+++ Domain/Models/UserCredential.cs
@@ -1,11 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
-
-namespace Domain.Models;
+﻿namespace Domain.Models;
 
 public class UserCredential
 {
-    [JsonPropertyName("user_name")]
     public string UserName { get; set; }
     
     public string Password { get; set; }
